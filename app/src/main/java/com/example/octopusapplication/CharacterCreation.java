bab131cae57e5785169d1e88529e9214afd99d3a
HGalloway <hilliardgalloway@gmail.com>
2021-04-09 19:22:22 -0500

    .

--- app/src/main/java/com/example/octopusapplication/CharacterCreation.java
+++ app/src/main/java/com/example/octopusapplication/CharacterCreation.java
@@ -10,5 +10,6 @@ public class CharacterCreation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.character_creation);
+        System.out.println("Yeet");
     }
 }
